2f246036a34273f5de909c4c26a0bbd4811547ad
hpatel826 <curlefry@gmail.com>
2014-12-01 20:27:41 -0500

    fix /poe/ endpoints, final poe.html

--- html/poe/deletePOE.php
+++ html/poe/deletePOE.php
@@ -3,25 +3,23 @@ include(dirname(__FILE__)."/../libs/shield_DB_info.php");
 include(dirname(__FILE__)."/../libs/shield_PHP_lib.php");
 
 $name=$_GET['poeName'];
-$sysname=$_GET['systemName'];
-
 
 $con = mysqli_connect($mysql_host,$mysql_user,$mysql_password,$mysql_database) or die(shield_output('None',0,'Database Connection Error'));
 $res = mysqli_query($con,"SELECT * FROM PointsOfEntry WHERE EntryName='$name'");
 	
-if($name==null or $sysname==null){
-	shield_output('None',0,'Missing Inputs');
+if($name==null){
+	shield_output('None',0,'Missing poeName');
 }
 else{
 	if($res){
 		if(mysqli_num_rows($res) == 0){
-			$query=
-			"INSERT INTO PointsOfEntry (EntryName,SystemName) VALUES ('$name','$sysname');";
-			mysqli_query($con,$query);
-			shield_output("Entry $name Created",1,'None');
+			shield_output('None',0,'POE Not Found');
 		}
 		else{
-			shield_output('None',0,'Duplicate POE Found');
+			$query=
+			"DELETE FROM PointsOfEntry WHERE EntryName='$name';";
+			mysqli_query($con,$query);
+			shield_output("Entry $name deleted",1,'None');
 		}
 	}
 	else{
